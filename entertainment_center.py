850ecb91022cb0e7e2a5316a0922851633a737dd
OliviaMessiah <oliviakingg20@gmail.com>
2016-08-15 12:32:08 -0500

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,22 @@
+import media
+import fresh_tomatoes
+
+FRIDAY = media.Movie("Friday",
+                            "https://en.wikipedia.org/wiki/Friday_(1995_film)"
+                            "http://t0.gstatic.com/images?q=tbn:ANd9GcQWZBGqc0aGK04BzbT4mFCZXgO5acmsCOKesWr-HZZeWiUce1Fq"
+                            "215px-http://t0.gstatic.com/images?q=tbn:ANd9GcQWZBGqc0aGK04BzbT4mFCZXgO5acmsCOKesWr-HZZeWiUce1Fq",
+                            "https://www.youtube.com/watch?v=nH1Ulp4PBtA",
+                            "Marcus Raboy",
+                            "Ice Cube, Chris Tucker, Mike Epps",
+                            "1995", False, None)
+
+
+MAD_WOMEN = media.Movie("Diary of mad black women",
+                            "https://en.wikipedia.org/wiki/Diary_of_a_Mad_Black_Woman"
+                            "http://www.gstatic.com/tv/thumb/movieposters/86339/p86339_p_v8_ad.jpg"
+                            "220px-https://images-na.ssl-images-amazon.com/images/I/51CwtbkJZeL.jpg",
+                            "https://www.youtube.com/watch?v=13BiV4sIMGM",
+                            "Darren Grant",
+                            "Kimberly Elise, Shemar moore, Steve Harris",
+                            "2005", False, None)
+
