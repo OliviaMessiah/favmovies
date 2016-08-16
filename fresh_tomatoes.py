02bc16abf6a0a5a33f23d09d120618f9c881644b
OliviaMessiah <oliviakingg20@gmail.com>
2016-08-16 08:08:20 -0500

    Update fresh_tomatoes.py

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -130,7 +130,7 @@ movie_tile_content = '''
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
-    for movie in movies:
+    for Movie in Movies:
         # Extract the youtube ID from the url
         youtube_id_match = re.search(
             r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
@@ -143,7 +143,7 @@ def create_movie_tiles_content(movies):
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            movie.trailer_youtube_id=movie.trailer_youtube_id
         )
     return content
 
