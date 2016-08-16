a71ae7d04ffe1ef343b0bc09eb8ff7cbb32bf004
OliviaMessiah <oliviakingg20@gmail.com>
2016-08-16 08:06:27 -0500

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,8 +2,8 @@ import media
 import fresh_tomatoes
 
 FRIDAY = media.Movie("Friday",
-                            "https://en.wikipedia.org/wiki/Friday_(1995_film)"
-                            "http://t0.gstatic.com/images?q=tbn:ANd9GcQWZBGqc0aGK04BzbT4mFCZXgO5acmsCOKesWr-HZZeWiUce1Fq"
+                            "https://en.wikipedia.org/wiki/Friday_(1995_film)",
+                            "http://t0.gstatic.com/images?q=tbn:ANd9GcQWZBGqc0aGK04BzbT4mFCZXgO5acmsCOKesWr-HZZeWiUce1Fq",
                             "215px-http://t0.gstatic.com/images?q=tbn:ANd9GcQWZBGqc0aGK04BzbT4mFCZXgO5acmsCOKesWr-HZZeWiUce1Fq",
                             "https://www.youtube.com/watch?v=nH1Ulp4PBtA",
                             "Marcus Raboy",
@@ -12,8 +12,8 @@ FRIDAY = media.Movie("Friday",
 
 
 MAD_WOMEN = media.Movie("Diary of mad black women",
-                            "https://en.wikipedia.org/wiki/Diary_of_a_Mad_Black_Woman"
-                            "http://www.gstatic.com/tv/thumb/movieposters/86339/p86339_p_v8_ad.jpg"
+                            "https://en.wikipedia.org/wiki/Diary_of_a_Mad_Black_Woman",
+                            "http://www.gstatic.com/tv/thumb/movieposters/86339/p86339_p_v8_ad.jpg",
                             "220px-https://images-na.ssl-images-amazon.com/images/I/51CwtbkJZeL.jpg",
                             "https://www.youtube.com/watch?v=13BiV4sIMGM",
                             "Darren Grant",
